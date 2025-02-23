86e2c4fc38657716d1a221d4704535f0a6522eed
vishnudude14 <128147325+vishnudude14@users.noreply.github.com>
2025-02-24 01:10:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 import logging.config
 from aiohttp import web
-from plugins.web_server import web_server
+from plugins import web_server
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
