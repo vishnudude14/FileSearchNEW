a058ad657a3fcdf9104881c018d6846383b7f129
vishnudude14 <128147325+vishnudude14@users.noreply.github.com>
2025-03-06 23:54:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,24 @@ from Script import script
 from datetime import date, datetime
 import pytz
 
+import asyncio
+import aiohttp
+
+URL = "https://nuclear-dove-tgcinema-022ee3d2.koyeb.app/"  # Replace with your koyeb app link...
+
+async def ping():
+    async with aiohttp.ClientSession() as session:
+        while True:
+            try:
+                async with session.get(URL) as response:
+                    print(f"Pinged server, status: {response.status}")
+            except Exception as e:
+                print(f"{e}")
+            await asyncio.sleep(120)
+
+loop = asyncio.get_event_loop()
+loop.create_task(ping())
+
 class Bot(Client):
 
     def __init__(self):
